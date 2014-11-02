98544c11e8b791d9407aee1851df39d8a7e84ed2
wadepeng@163.com <zp2424068>
2014-11-02 13:49:52 +0800

    make changes
    Change-Id: Idb8961211e86b891f71c7af7fd1b8ebb6d9d7879
    Signed-off-by: wadepeng@163.com <zp2424068>

---
+++ src/siem-correlation-engine/AppMain.cpp
@@ -0,0 +1,25 @@
+/**
+ *   @file   AppMain.cpp
+ *   @brief  brief
+ *
+ *   detail
+ *
+ *   @internal www.captech.net.cn
+ *   author:    zhangpeng
+ *   mail:      zhangpeng@captech.net.cn,developer.zhang.peng@gmail.com
+ *   Created:   Jul 5, 2012
+ *   Revision:  1.0.0
+ *   Compiler:  gcc/g++
+ *   Company:   Captech Co., Ltd.
+ *   Copyright: Copyright (c) 2008, Captech Co., Ltd.
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
